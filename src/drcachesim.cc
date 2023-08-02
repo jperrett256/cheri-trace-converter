4a7088bf4a40a7bd92536153dfbdd3cf71b5e78e
Joshua Perrett <jperrett256@gmail.com>
2023-08-02 18:21:06 +0100

    cleanup: removed use of designated initializers in C++ code
    Non-standard C++, older versions of g++ do not appear to support it.

--- src/drcachesim.cc
+++ src/drcachesim.cc
@@ -17,35 +17,36 @@ void write_drcachesim_header(trace_writer_t * writer)
     assert(writer);
 
     // add header and other initial marker entries
-    trace_entry_t header =
-    {
-        .type = TRACE_TYPE_HEADER,
-        .size = 0,
-        .addr = TRACE_ENTRY_VERSION
-    };
+    trace_entry_t header = {0};
+    header.type = TRACE_TYPE_HEADER;
+    header.size = 0;
+    header.addr = TRACE_ENTRY_VERSION;
 
     // NOTE only have one process and one thread at the moment
-    trace_entry_t thread = { .type = TRACE_TYPE_THREAD, .size = 4, .addr = 1 };
-    trace_entry_t pid = { .type = TRACE_TYPE_PID, .size = 4, .addr = 1 };
-    trace_entry_t timestamp =
-    {
-        .type = TRACE_TYPE_MARKER,
-        .size = TRACE_MARKER_TYPE_TIMESTAMP,
-        .addr = 0
-    };
-    trace_entry_t cpuid =
-    {
-        .type = TRACE_TYPE_MARKER,
-        .size = TRACE_MARKER_TYPE_CPU_ID,
-        .addr = 0
-    };
+    trace_entry_t thread = {0};
+    thread.type = TRACE_TYPE_THREAD;
+    thread.size = 4;
+    thread.addr = 1;
 
-    trace_entry_t page_size =
-    {
-        .type = TRACE_TYPE_MARKER,
-        .size = TRACE_MARKER_TYPE_PAGE_SIZE,
-        .addr = PAGE_SIZE
-    };
+    trace_entry_t pid = {0};
+    pid.type = TRACE_TYPE_PID;
+    pid.size = 4;
+    pid.addr = 1;
+
+    trace_entry_t timestamp = {0};
+    timestamp.type = TRACE_TYPE_MARKER;
+    timestamp.size = TRACE_MARKER_TYPE_TIMESTAMP;
+    timestamp.addr = 0;
+
+    trace_entry_t cpuid = {0};
+    cpuid.type = TRACE_TYPE_MARKER;
+    cpuid.size = TRACE_MARKER_TYPE_CPU_ID;
+    cpuid.addr = 0;
+
+    trace_entry_t page_size = {0};
+    page_size.type = TRACE_TYPE_MARKER;
+    page_size.size = TRACE_MARKER_TYPE_PAGE_SIZE;
+    page_size.addr = PAGE_SIZE;
 
     trace_writer_emit(writer, &header, sizeof(header));
     trace_writer_emit(writer, &thread, sizeof(thread));
@@ -59,12 +60,10 @@ void write_drcachesim_footer(trace_writer_t * writer)
 {
     assert(writer);
 
-    trace_entry_t footer =
-    {
-        .type = TRACE_TYPE_FOOTER,
-        .size = 0,
-        .addr = 0
-    };
+    trace_entry_t footer = {0};
+    footer.type = TRACE_TYPE_FOOTER;
+    footer.size = 0;
+    footer.addr = 0;
 
     trace_writer_emit(writer, &footer, sizeof(footer));
 }
@@ -105,12 +104,10 @@ static void write_drcachesim_tag_entry(trace_writer_t * writer, uint8_t type, ui
 
         assert(tag == 0 || tag == 1);
 
-        trace_entry_t tag_entry =
-        {
-            .type = TRACE_TYPE_MARKER,
-            .size = TRACE_MARKER_TYPE_TAG_CHERI,
-            .addr = tag
-        };
+        trace_entry_t tag_entry = {0};
+        tag_entry.type = TRACE_TYPE_MARKER;
+        tag_entry.size = TRACE_MARKER_TYPE_TAG_CHERI;
+        tag_entry.addr = tag;
 
         trace_writer_emit(writer, &tag_entry, sizeof(tag_entry));
     }
@@ -118,18 +115,15 @@ static void write_drcachesim_tag_entry(trace_writer_t * writer, uint8_t type, ui
 
 static void write_drcachesim_page_mapping_entries(trace_writer_t * writer, u64 vaddr, u64 paddr)
 {
-    trace_entry_t paddr_mapping_entry =
-    {
-        .type = TRACE_TYPE_MARKER,
-        .size = TRACE_MARKER_TYPE_PHYSICAL_ADDRESS,
-        .addr = paddr
-    };
-    trace_entry_t vaddr_mapping_entry =
-    {
-        .type = TRACE_TYPE_MARKER,
-        .size = TRACE_MARKER_TYPE_VIRTUAL_ADDRESS,
-        .addr = vaddr
-    };
+    trace_entry_t paddr_mapping_entry = {0};
+    paddr_mapping_entry.type = TRACE_TYPE_MARKER;
+    paddr_mapping_entry.size = TRACE_MARKER_TYPE_PHYSICAL_ADDRESS;
+    paddr_mapping_entry.addr = paddr;
+
+    trace_entry_t vaddr_mapping_entry = {0};
+    vaddr_mapping_entry.type = TRACE_TYPE_MARKER;
+    vaddr_mapping_entry.size = TRACE_MARKER_TYPE_VIRTUAL_ADDRESS;
+    vaddr_mapping_entry.addr = vaddr;
 
     trace_writer_emit(writer, &paddr_mapping_entry, sizeof(paddr_mapping_entry));
     trace_writer_emit(writer, &vaddr_mapping_entry, sizeof(vaddr_mapping_entry));
@@ -166,12 +160,10 @@ void write_drcachesim_trace_entry_vaddr(trace_writer_t * writer, map_u64 page_ta
         // TODO skip these instead?
         printf("ERROR: missing paddr for first access to page (vaddr: %lu)\n", vaddr);
 
-        trace_entry_t no_mapping_entry =
-        {
-            .type = TRACE_TYPE_MARKER,
-            .size = TRACE_MARKER_TYPE_PHYSICAL_ADDRESS_NOT_AVAILABLE,
-            .addr = vaddr
-        };
+        trace_entry_t no_mapping_entry = {0};
+        no_mapping_entry.type = TRACE_TYPE_MARKER;
+        no_mapping_entry.size = TRACE_MARKER_TYPE_PHYSICAL_ADDRESS_NOT_AVAILABLE;
+        no_mapping_entry.addr = vaddr;
 
         trace_writer_emit(writer, &no_mapping_entry, sizeof(no_mapping_entry));
     }
